21cd70d63658d0b4346b3aefd01d9beeca0baa46
Ekta <ektaarora3501@gmail.com>
2020-02-05 18:21:08 +0530

    Adding urls for payment page

--- fintech/payment/urls.py
+++ fintech/payment/urls.py
@@ -8,11 +8,16 @@ urlpatterns =[
      path('signup/',views.Signup,name="register_user"),
      path('verify_phone/<code>/<phone>',views.verify_ph,name='verify_phone'),
      path('send/<phone>',views.Send,name="send_otp"),
-     path('front/images/<username>',views.CameraLand,name="camera_landing"),
-     path('get/images/<username>',views.CameraRecord,name="camera_record"),
+     path('images/<username>',views.CameraLand,name="camera_landing"),
+     path('get/images/front/<username>',views.CameraFront,name="camera_record"),
+     path('get/images/sideways/<username>',views.CameraSide,name="camera_sideways"),
      path('login',views.Login,name='login'),
      path('dashboard/<code>/<user>',views.Dashboard,name="dashboard"),
      path('logout/<user>',views.Logout,name='logout_user'),
+     path('payment_dashboard',views.PayDashboard,name="payment_dashboard"),
+     path('payment_page/usercase',views.Pay,name="payment_page"),
+     path('payment_verify/<code>/<username>',views.PaymentVerify,name="payment_verify"),
+     path('payment_details/<code>/<username>',views.PaymentDetails,name="payment_details"),
 
 
 ]
